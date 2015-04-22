3571d45af310b1c1d824697bb8d7ec1d2fa4b360
Bryn Stringer <kaos.nyrb@gmail.com>
2015-04-22 17:08:57 +0100

    Update index.js
    Moving it to port 80

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
 function guid() {
